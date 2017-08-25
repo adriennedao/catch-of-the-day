fafb0857467d7659c00e33c0effd9888e8729097
AdrienneD <adriennedao@gmail.com>
2017-08-24 23:24:45 -0400

    initial commit

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB6TntYcZVxfcg_iqC2sQKiMbkc2s_vAfY",
+      authDomain: "catch-of-the-day-adrienne-dao.firebaseapp.com",
+      databaseURL: "https://catch-of-the-day-adrienne-dao.firebaseio.com",
+
+});
+
+export default base;
